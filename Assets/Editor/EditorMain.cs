45df8888c0694112185041a538163ab73e5fad81
SilasPeters <silaspeters03@gmail.com>
2020-08-15 17:18:05 +0200

    First

---
+++ Assets/Editor/EditorMain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class EditorMain : EditorWindow
+{
+    [MenuItem("Window/Custom Editor")]
+    public static void ShowGUI()
+    {
+        GetWindow<EditorMain>("Custom Editor");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("BIER");
+    }
+}
